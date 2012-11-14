8a4a30a72fc6c73f467247033c9ceac7c3e094b8
PRASEN <PRASEN@PRASEN-PC>
2012-11-14 23:46:25 +0530

    sdf

--- szdsdv/src/sdv/Main.java
+++ szdsdv/src/sdv/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Another message");
+		// System.out.println("Another message"); commented at branch 1
 		System.out.println("Some more changes");
 		System.out.println("Changed in branch one");
 	}
