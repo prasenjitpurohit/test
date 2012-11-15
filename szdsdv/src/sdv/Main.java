e58d5519e95736ba1b9d5716fa58a8f8d16fd803
prasenjitpurohit <github@prasenjit.me>
2012-11-15 20:50:16 +0530

    sdfsdf

--- szdsdv/src/sdv/Main.java
+++ szdsdv/src/sdv/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		// System.out.println("Another message"); commented at branch 1
+		// System.out.println("Another message"); commented at branch 1 added at master from web
 		System.out.println("Some more changes");
 		System.out.println("Changed in branch one");
 	}
